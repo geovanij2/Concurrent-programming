5ce149d53d46681a2067ada1d9b6b0b7d2c2abaf
DanielScariot <faroeste.telefone@gmail.com>
2017-05-18 22:18:30 -0300

    V 2.1 Daniel Scariot
    Confia que agora vai!
    Arrumado alguns problemas com alocação de board.

--- Project1/GoL_Threads.c
+++ Project1/GoL_Threads.c
@@ -13,10 +13,12 @@
 *
 */
 
-/* Versão 1.0
+/* Versão 2.0
 * Adicionado as variáveis globais
 * Feito a leitura e a verificação do argumento passado via terminal
 * Criação das threads
+* Utilizado exclusão mútua dentro das threads
+* Barreira para sincronização
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,7 +27,7 @@
 typedef unsigned char cell_t;
 
 //Variaveis globais
-cell_t **prev, **next, **tmp, **newboard, **board;
+cell_t **prev, **next, **tmp;
 int maxth, steps, cont = 0, size;
 pthread_mutex_t m;
 pthread_barrier_t barreira;
@@ -72,19 +74,19 @@ void *play () {
         pthread_barrier_wait (&barreira);     //a thread para na barreira e espera todas as outras threads entrarem no mesmo caso,
                               //ou seja, espera que a ultima thread que ainda está executando a ultima coluna termine
           pthread_exit(NULL);             //após todas as threads acabarem elas encerram
-      }
-      pthread_mutex_lock(&m);             //Exclusão mútua para que
-      cont++;                     //Se uma thread fazer cont++ (que indica que a coluna cont -1 foi coletada) ela precisa ter certeza que 
-      i = cont-1;                   //nenhuma outra thread execute essa linha, pois precisa garantir que o valor de i represente a coluna que foi coletada
-      pthread_mutex_unlock(&m);           //Final da exclusão mútua
-      for (j=0; j<size; j++) {            //Após a thread coletar uma coluna, ele percorre todas as linhas dessa coluna e verifica o que fazer com ela
-        a = adjacent_to (board, size, i, j);
-        if (a == 2) newboard[i][j] = board[i][j];
-        if (a == 3) newboard[i][j] = 1;
-        if (a < 2) newboard[i][j] = 0;
-        if (a > 3) newboard[i][j] = 0;
-      } 
-  }
+      } else {
+        pthread_mutex_lock(&m);             //Exclusão mútua para que
+        cont++;                     //Se uma thread fazer cont++ (que indica que a coluna cont -1 foi coletada) ela precisa ter certeza que 
+        i = cont-1;                   //nenhuma outra thread execute essa linha, pois precisa garantir que o valor de i represente a coluna que foi coletada
+        pthread_mutex_unlock(&m);           //Final da exclusão mútua
+        for (j=0; j<size; j++) {            //Após a thread coletar uma coluna, ele percorre todas as linhas dessa coluna e verifica o que fazer com ela
+          	a = adjacent_to (prev, size, i, j);
+          	if (a == 2) next[i][j] = prev[i][j];
+          	if (a == 3) next[i][j] = 1;
+          	if (a < 2) next[i][j] = 0;
+          	if (a > 3) next[i][j] = 0;
+      	} 
+  	}
 }
 
 /* print the life board */
@@ -122,22 +124,29 @@ void read_file (FILE * f, cell_t ** board, int size) {
 int main (int argc, char *argv[]) {
   int i, j;
   pthread_mutex_init(&m,NULL); 
-  maxth = argc > 1? atoi(argv[1]) : 1;  // leitura do argumento
-  FILE    *f;
+  // ---------- Leitura do argumento ---------- //
+  maxth = argc > 1? atoi(argv[1]) : 1;
+  // ---------- Leitura do file ---------- //
+  
+  FILE *f;
   f = stdin;
   fscanf(f,"%d %d", &size, &steps);
   cell_t ** prev = allocate_board (size);
   read_file (f, prev,size);
   fclose(f);
+  // ---------- Alocação ---------- //
+  
   cell_t ** next = allocate_board (size);
   cell_t ** tmp;
+  
   #ifdef DEBUG
   printf("Initial:\n");
   print(prev,size);
   #endif
   // ---------- Criação de ''maxth'' threads ---------- //
   pthread_t threads[maxth];
-  pthread_barrier_init (&barreira, NULL, maxth+1);  //Criação da barreira com o número de threads mais a main thread
+  pthread_barrier_init (&barreira, NULL, maxth);  //Criação da barreira com o número de threads mais a main thread
+  
   for (i=0; i<maxth; i++) {
     pthread_create(&threads[i], NULL, play, NULL);
   }
@@ -146,7 +155,7 @@ int main (int argc, char *argv[]) {
     for (j=0; j<maxth; j++){        // for para dar join em todas as threads e iniciar o GoL
         pthread_join(threads[j], NULL);  
       }
-      pthread_barrier_wait (&barreira);   // APÓS dar join em todas as threads ele só continuará o código após todas as threads acabarem!!!
+      //pthread_barrier_wait (&barreira);   // APÓS dar join em todas as threads ele só continuará o código após todas as threads acabarem!!!
                           // A main só voltara a rodar depois de todas as threads serem executadas
                         // Apartir dessa parte do código todas as threads estarão rodando
                         // elas irão parar apenas quando a barreira permitir, ou seja, quando todas as threads perceberem que todas as colunas foram processadas
@@ -154,6 +163,7 @@ int main (int argc, char *argv[]) {
       printf("%d ----------\n", i + 1);
       print (next,size);
       #endif
+      
       tmp = next;
       next = prev;
       prev = tmp;
